4b06706f6118bb178f2485aec96962984d932a86
Harry du Rails Heroes Course <harry.jmg@gmail.com>
2018-05-15 13:11:52 +0000

    ok

---
+++ app/bot/youtube.rb
@@ -0,0 +1,9 @@
+# app/bot/example.rb
+
+include Facebook::Messenger
+
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN'])
+
+Bot.on :message do |message|
+  message.reply(text: 'Hello, human!')
+end
